e738d03100f71a3531052276da9ce741068bb968
Pedro Venereo Padilla <103296054+Ctrlv-Package@users.noreply.github.com>
2022-08-03 18:46:17 -0400

    Update DemoG1.java
    removed test code

--- src/team/g1/DemoG1.java
+++ src/team/g1/DemoG1.java
@@ -6,18 +6,8 @@ public class DemoG1 {
 		System.out.println("Test");
 		System.out.println("Let's go team G1");
 		
-		System.out.println("Test2");
-		
-		System.out.println("Test3");
-		
-		System.out.println("Test4");
-		
-		System.out.println("Test5");
-		
-		System.out.println("Test6");
 		
 		System.out.println("Started from the bottom now we are here");
-		System.out.println("Test7");
 		
 		System.out.println("Java is fun");
 		System.out.println(" Carlos wants to get Lambotghini car so bad!!!");
@@ -28,8 +18,6 @@ public class DemoG1 {
 		//User Carlos
 		System.out.println("Tuesday Night Homework");
 		
-		// Hanouna
-		System.out.println("Test 8");
 
 		System.out.println("How's team G1 doing today?");
 		
