33b691d99d2b57322df673bea3a9dec3f3026469
cbatres522 <110418863+cbatres522@users.noreply.github.com>
2022-08-02 21:49:45 -0500

    Update DemoG1.java
    added print out comment by user carlos

--- src/team/g1/DemoG1.java
+++ src/team/g1/DemoG1.java
@@ -17,6 +17,9 @@ public class DemoG1 {
 		System.out.println("Test6");
 		
 		System.out.println("Started from the bottom now we are here");
+		
+		//User carlos
+		System.out.println("$150k salary plz");
 	}
 
 	
