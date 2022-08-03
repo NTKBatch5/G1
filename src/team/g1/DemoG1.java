2f2b963da6b8001e94ae37dcb0b0e6a11231c3e1
pedro <pedro@DESKTOP-1EHHS4J>
2022-08-02 21:44:36 -0400

    Test2

--- src/team/g1/DemoG1.java
+++ src/team/g1/DemoG1.java
@@ -1,12 +1,14 @@
 package team.g1;
 
 public class DemoG1 {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Test");
 		System.out.println("Let's go team G1");
+		
+		System.out.println("Test2");
 	}
-	
+
 	public static void add(String g) {
 		g = "G1";
 		System.out.println(g);
