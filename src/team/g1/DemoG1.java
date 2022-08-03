ee75d3251e1b28b94d2fd3a534d814ffb8d7d473
pedro <pedro@DESKTOP-1EHHS4J>
2022-08-03 19:12:38 -0400

    Error

--- src/team/g1/DemoG1.java
+++ src/team/g1/DemoG1.java
@@ -5,36 +5,33 @@ public class DemoG1 {
 	public static void main(String[] args) {
 		System.out.println("Test");
 		System.out.println("Let's go team G1");
-		
-		
+
 		System.out.println("Started from the bottom now we are here");
-		
+
 		System.out.println("Java is fun");
 		System.out.println(" Carlos wants to get Lambotghini car so bad!!!");
-		
-		// Pedro 
+
+		// Pedro
 		System.out.println("I want a pizza so bad right now..");
-		
-		//User Carlos
+
+		// User Carlos
 		System.out.println("Tuesday Night Homework");
-		
 
 		System.out.println("How's team G1 doing today?");
-		
+
 		System.out.println("Fixing....");
-		
+
 		System.out.println("Hello Batch 7....");
 	}
 
 	public static void sub(int a, int b) {
 		System.out.println(a - b);
-		
-			//User Carlos
+
+		// User Carlos
 		System.out.println("Tuesday Night Homework");
-		
 
 		System.out.println("How's team G1 doing today?");
-		
+
 		System.out.println("Fixing....");
 	}
 }
